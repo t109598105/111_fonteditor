c4620ae0352d4cde83942b0f71280d5f29a7496f
poynt2005 <poynt2010@hotmail.com>
2022-06-15 18:44:22 +0800

    First Commit

---
+++ main.js
@@ -0,0 +1,85 @@
+window.addEventListener('load', function(){
+    var renderContent01 = function(data){
+
+        var container = d3
+            .select('#main-content')
+            .selectAll('div')
+            .data(data)
+                .enter()
+                .append('div')
+        ;
+
+
+
+        container
+            .append('hr')
+            .attr('class', 'section-heading-spacer')
+        ;
+
+        var row = container
+            .append('div')
+            .attr('class', 'row')
+        ;
+
+        var col = row
+            .append('div')
+            .attr('class', 'col-lg-5 col-sm-6')
+        ;
+
+        col
+            .append('h2')
+            .attr('class', 'section-heading')
+            .text(function(d){ return d.title; })
+        ;
+
+        col
+            .selectAll('a')
+            .data(function(d){ return d.urls })
+            .enter()
+                .append('a')
+                .style('display', 'block')
+                .style('font-size', '2rem')
+                .style('text-decoration', 'none')
+                .attr('href', function(d){ return d.href; })
+                .text(function(d){ return d.text; })
+        ;
+
+        col
+            .append('p')
+            .attr('class', 'font18')
+            .text(function(d){ return d.content; })
+        ;
+
+        row
+            .append('div')
+            .attr('class', 'col-md-6 col-sm-6 col-xs-12 d-flex justify-center align-center')
+            .style('height', '50%')
+            .style('border-style', 'solid')
+            .style('border-radius', '10px')
+            .style('border-width', '1px')
+                .selectAll('div')
+                .data(function(d){ return [d]; })
+                .enter()
+                    .append('div')
+                    .attr('class', 'single_abouts wow fadeInRight')
+                    .attr('data-wow-duration', '1s')
+                        .append('img')
+                        .attr('src', function(d){ return './img/' + d.pic; })
+                        .style('width', '100%')
+                        .style('max-height', '100%')
+                        .attr('alt', function(d){ return d.title; })
+        ;
+
+
+
+    };
+    
+    
+    d3.request('./content.json')
+        .responseType('json')
+        .get(function(data){
+            renderContent01(data.response);
+        })
+    ;
+
+});
---
+++ readme.md
@@ -0,0 +1,6 @@
+# 111_fonteditor
+
+論文中文標題: 基於網頁字型變形與編輯系統  
+Paper English Title: Web-Based Font Morphing and Editing System  
+
+論文主頁面: [連結](https://paper.poynt2005.top/)  
